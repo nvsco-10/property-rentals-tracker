13468ba555f37c2d3d9d7de7720bde45e07ebc1d
Neema Velasco <neemavelasco@gmail.com>
2022-06-09 15:47:02 -0500

    fixes

--- server/server.js
+++ server/server.js
@@ -31,14 +31,13 @@ if(process.env.NODE_ENV !== 'production'){
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
 app.use(express.json())
 // app.use(helmet())
 // app.use(xss())
 // app.use(mongoSanitize())
-
-app.get('/', (req,res) => {
-  res.send('Welcome')
-})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/rentals', authenticateUser, rentalsRouter)
